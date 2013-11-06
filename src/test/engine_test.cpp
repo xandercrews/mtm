931a8024438f871286e598bf291af0486feb4dd2
dhardman <daniel.hardman@gmail.com>
2013-11-05 20:41:52 -0700

    add another unit test for follower mode

--- src/test/engine_test.cpp
+++ src/test/engine_test.cpp
@@ -18,4 +18,13 @@ TEST(engine_test, leader_ctor_works) {
 	cmdline cmdline(3, args);
 	engine_handle engine = factory.make(cmdline);
 	ASSERT_TRUE(static_cast<bool>(engine));
+	ASSERT_FALSE(engine->is_follower());
+}
+
+TEST(engine_test, follower_ctor_works) {
+	char const * args[] = {"progname", "--talkport", "2500", "--follow"};
+	cmdline cmdline(4, args);
+	engine_handle engine = factory.make(cmdline);
+	ASSERT_TRUE(static_cast<bool>(engine));
+	ASSERT_TRUE(engine->is_follower());
 }
